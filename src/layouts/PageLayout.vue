6a4b7c0f423cfa03b634dff59119dbf5c49b49b5
Dodi Aditya <dodiaridharma@gmail.com>
2025-06-17 18:51:26 +0800

    chore: add assets, clear starter template, and setup scss

---
+++ src/layouts/PageLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MainFooter from './MainFooter.vue'
+import MainNavbar from './MainNavbar.vue'
+</script>
+
+<template>
+  <MainNavbar />
+  <slot />
+  <MainFooter />
+</template>
---
+++ src/views/OrderView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>ORDER VIEW</div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  color: $primary-color;
+}
+</style>
