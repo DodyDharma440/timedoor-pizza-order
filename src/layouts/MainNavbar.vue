3ed1254f37d684e85e327da248b78ee4a6f24bae
Dodi Aditya <dodiaridharma@gmail.com>
2025-06-19 20:28:06 +0800

    feat: mobile navbar menu

--- src/layouts/MainNavbar.vue
+++ src/layouts/MainNavbar.vue
@@ -4,6 +4,11 @@ import { onMounted, onUnmounted, ref } from 'vue'
 import { RouterLink } from 'vue-router'
 
 const solidNav = ref(false)
+const isOpenMobileMenu = ref(false)
+
+const toggleMobileMenu = () => {
+  isOpenMobileMenu.value = !isOpenMobileMenu.value
+}
 
 const handleScroll = () => {
   solidNav.value = window.scrollY > 500
@@ -24,6 +29,7 @@ onUnmounted(() => {
       <div class="navbar__logo">
         <img src="/images/logo.svg" />
       </div>
+
       <div class="navbar__menu">
         <RouterLink v-for="(menu, index) in navMenu" :to="menu.path" :key="index">
           <span>{{ menu.label }}</span>
@@ -33,11 +39,47 @@ onUnmounted(() => {
         <button class="btn navbar__buttons-login">Login</button>
         <button class="btn btn--primary navbar__buttons-register">Register</button>
       </div>
+
+      <button class="navbar__mobile-btn" @click="toggleMobileMenu">
+        <img src="/images/icons/menu.svg" class="navbar__mobile-btn-icon" alt="menu" />
+      </button>
     </div>
+
+    <Teleport to="body">
+      <div class="navbar__mobile-menu" :class="{ 'navbar__mobile-menu-opened': isOpenMobileMenu }">
+        <button class="navbar__mobile-menu-close" @click="toggleMobileMenu">
+          <img src="/images/icons/close.svg" class="navbar__mobile-menu-close-icon" alt="menu" />
+        </button>
+
+        <RouterLink v-for="(menu, index) in navMenu" :to="menu.path" :key="index">
+          <span>{{ menu.label }}</span>
+        </RouterLink>
+
+        <div class="navbar__buttons navbar__buttons--mobile">
+          <button class="btn navbar__buttons-login">Login</button>
+          <button class="btn btn--primary navbar__buttons-register">Register</button>
+        </div>
+      </div>
+    </Teleport>
   </nav>
 </template>
 
 <style lang="scss" scoped>
+@mixin link-styles {
+  a {
+    text-decoration: none;
+    color: inherit;
+
+    &:hover {
+      color: $primary-color;
+    }
+
+    &.router-link-active {
+      color: $primary-color;
+    }
+  }
+}
+
 .navbar {
   position: fixed;
   top: 0;
@@ -59,7 +101,8 @@ onUnmounted(() => {
     }
   }
 
-  &--solid {
+  &--solid,
+  &__mobile-menu {
     background-color: rgba($color: #000000, $alpha: 0.5);
     backdrop-filter: blur(10px);
   }
@@ -99,18 +142,7 @@ onUnmounted(() => {
       gap: spacing(7.5);
     }
 
-    a {
-      text-decoration: none;
-      color: inherit;
-
-      &:hover {
-        color: $primary-color;
-      }
-
-      &.router-link-active {
-        color: $primary-color;
-      }
-    }
+    @include link-styles;
   }
 
   &__buttons {
@@ -122,6 +154,15 @@ onUnmounted(() => {
       display: flex;
     }
 
+    &--mobile {
+      display: flex;
+      margin-top: spacing(4);
+      width: 100%;
+      .btn {
+        flex: 1;
+      }
+    }
+
     &-login {
       &:hover {
         color: $primary-color;
@@ -130,6 +171,61 @@ onUnmounted(() => {
 
     &-register {
       min-width: 160px;
+    }
+  }
+
+  &__mobile-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @include lg {
+      display: none;
+    }
+
+    &-icon {
+      width: 32px;
+      height: 32px;
+      object-fit: contain;
+    }
+  }
+
+  &__mobile-menu {
+    color: #ffffff;
+    padding-top: 72px;
+    padding-left: spacing(6);
+    padding-right: spacing(6);
+    padding-bottom: spacing(10);
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: to-rem(24);
+    gap: spacing(6);
+    z-index: $navbar-z-index + 1;
+    transform: translateY(-100%);
+    transition: transform 300ms ease;
+
+    @include link-styles;
+
+    &-close {
+      position: absolute;
+      top: spacing(6);
+      right: spacing(6);
+
+      &-icon {
+        width: 24px;
+        height: 24px;
+        object-fit: contain;
+      }
+    }
+
+    &-opened {
+      transform: translateY(0);
     }
   }
 }
